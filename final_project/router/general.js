3501dc213718c83109a4274f2ce74ff218d8d13b
Dhivshan <dhivyalshan@gmail.com>
2025-02-05 23:46:15 -0500

    Updated files

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,11 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const getBooks = () => {
+    return new Promise((resolve, reject) => {
+        resolve(books);
+    });
+};
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -13,27 +18,41 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ // return res.status(300).json({message: "Yet to be implemented"});
+     res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN = req.params.isbn;
+    res.send(books[ISBN]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  getBooks()
+  .then((bookEntries) => Object.values(bookEntries))
+  .then((books) => books.filter((book) => book.author === author))
+  .then((filteredBooks) => res.send(filteredBooks));
 });
+
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  getBooks()
+  .then((bookEntries) => Object.values(bookEntries))
+  .then((books) => books.filter((book) => book.title === title))
+  .then((filteredBooks) => res.send(filteredBooks));
 });
-
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
